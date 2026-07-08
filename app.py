a1a7344d81ab0ff941576d5797818a36a19ca185
CVerde <cyril.verde@gmail.com>
2026-07-08 12:52:18 +0200

    Fix GPIO busy: disable reloader

--- app.py
+++ app.py
@@ -484,4 +484,4 @@ def get_haccp():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
